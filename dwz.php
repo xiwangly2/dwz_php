dcd50c460bd16ebf173c7459b5c5f2cd90735f3a
LY <1334850101@qq.com>
2020-07-23 15:12:45 +0800

    Add files via upload

--- dwz.php
+++ dwz.php
@@ -11,20 +11,40 @@ dwz.php
 //定义编码为utf-8
 header("content-type:text/html;charset=utf-8");
 //$x1定义保存的子文件夹，一般最短为根目录（如果这个网站绑定在子域中，你可以放心的改成根目录）
-$x1 = "./dwz/";
+$x1 = "";
 //$x2定义显示的子文件夹，一般最短为根目录（如果这个网站绑定在子域中，你可以放心的改成根目录）
-$x2 = "/dwz/";
+$x2 = "/";
+//计数文件位置及文件名
+$x3 = "number.txt";
+//计数的起始数字（保留短网址范围）
+$number_s = "1000";
 //get传入url
 $url = $_GET["url"];
-//非默认端口$is_port请填true
-$is_port = "false";
+//获取端口
+$port = $_SERVER["SERVER_PORT"];
 //追加开头文本，默认为空
 $prefix = "";
 $head = "http://";
-//分配随机字符串
-$sran = uniqid($prefix);
-//分配更长的随机字符串
+
+//分配字符串，方法①
+//$sran = uniqid($prefix);
+
+//分配更长的字符串，方法②
 //$sran = uniqid($prefix,TRUE);
+
+//分配数字，方法③
+$number = file_get_contents($x3);
+if($number == "")
+{
+	file_put_contents($x3,$number_s);
+	$number = file_get_contents($x3);
+}
+//移量（可以带浮点数）
+$number1 = $number+"1";
+$sran = $number1;
+//写入计数
+file_put_contents($x3,$number1);
+
 //核心部分，通过index默认文件+Location实现跳转（避免配置伪静态，但是更耗存储空间）
 $index_file = $x1.$sran."/index.php";
 $text = '<?php'."\n"."header(\"Location: $url\");"."\n".'?>';
@@ -35,13 +55,13 @@ if($url == "")
 mkdir($x1);
 mkdir($x1.$sran);
 file_put_contents($index_file,$text);
-//判断是否输出显示非默认端口，取决于$is_port
-if($is_port == "true")
+//判断是否输出显示非默认端口
+if($port == "80")
 {
-	echo $head.$_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$x2.$sran;
+	echo $head.$_SERVER["SERVER_NAME"].$x2.$sran;
 }
 else
 {
-	echo $head.$_SERVER["SERVER_NAME"].$x2.$sran;
+	echo $head.$_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$x2.$sran;
 }
 ?>
