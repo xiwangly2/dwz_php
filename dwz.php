7f1a3c7192885044fb932e1909fe7b72efb35d9e
LY <1334850101@qq.com>
2020-08-07 12:10:20 +0800

    Add files via upload

--- dwz.php
+++ dwz.php
@@ -2,46 +2,109 @@
 /**
 dwz.php
 用于生成短网址（当然，如果你的域名太长了，那可能就不叫短网址了）
-这个是隐性跳转的（可能更容易被举报），显性（动态）跳转暂时就不写了……
-搞在数据库的我就先不写了……
+这个是隐性跳转的（可能更容易被举报），显性（动态）跳转暂时就不写了
+如果跳转的地址带有get参数，请先将地址转码
+这是搞在数据库的
 懒……
 格式：http(s)://域名(:端口)/dwz.php?url=要生成短网址的url
 @xiwangly
+数据库编码推荐utf8mb4
+配置信息要改
 */
-//定义编码为utf-8
 header("content-type:text/html;charset=utf-8");
-//$x1定义保存的子文件夹，一般最短为根目录（如果这个网站绑定在子域中，你可以放心的改成根目录）
-$x1 = "./dwz/";
-//$x2定义显示的子文件夹，一般最短为根目录（如果这个网站绑定在子域中，你可以放心的改成根目录）
-$x2 = "/dwz/";
+//配置信息
+//$host = "host(:port)";
+$host = "";
+$username = "";
+$password = "";
+$dbname = "";
+$tablename = "";
+//get参数
+$url = $_GET["url"];
+//$x1定义显示的文件夹，一般最短为根目录
+$x1 = "/";
+//计数的起始数字（保留短网址范围）
+$number_s = "1000";
 //get传入url
 $url = $_GET["url"];
-//非默认端口$is_port请填true
-$is_port = "false";
-//追加开头文本，默认为空
-$prefix = "";
-$head = "http://";
-//分配随机字符串
-$sran = uniqid($prefix);
-//分配更长的随机字符串
-//$sran = uniqid($prefix,TRUE);
-//核心部分，通过index默认文件+Location实现跳转（避免配置伪静态，但是更耗存储空间）
-$index_file = $x1.$sran."/index.php";
-$text = '<?php'."\n"."header(\"Location: $url\");"."\n".'?>';
-if($url == "")
+if(!isset($url) || $url == "")
 {
 	die("Error!Please visit https://github.com/xiwangly2/dwz_php/blob/master/README.md");
 }
-mkdir($x1);
-mkdir($x1.$sran);
-file_put_contents($index_file,$text);
-//判断是否输出显示非默认端口，取决于$is_port
-if($is_port == "true")
+//获取端口
+$port = $_SERVER["SERVER_PORT"];
+//判断协议，如果要兼容非https请使用http
+$head = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
+//$head = "http://";
+//追加开头文本，默认为空
+//$prefix = "";
+
+//分配字符串，方法①
+//$sran = uniqid($prefix);
+
+//分配更长的字符串，方法②
+//$sran = uniqid($prefix,TRUE);
+
+//创建连接
+$conn = new mysqli($host,$username,$password,$dbname);
+//设置编码
+$sql = "DEFAULT CHARSET=utf8mb4";
+$conn->query($sql);
+//使用sql创建数据表
+$sql = "CREATE TABLE $tablename (id INT(64) UNSIGNED AUTO_INCREMENT PRIMARY KEY,url VARCHAR(128) NOT NULL,code VARCHAR(32) NOT NULL,reg_date TIMESTAMP)";
+$conn->query($sql);
+$sql = "SELECT id, url, code FROM $tablename";
+$conn->query($sql);
+$sql = "SELECT COUNT(*) FROM {$tablename}";
+$result = $conn->query($sql);
+$rowss = mysqli_fetch_row($result);
+
+//分配数字，方法③
+$number = $number_s+"0"+$rowss[0];
+//移量（可以带浮点数）
+$number1 = $number+"1";
+$sran = $number1;
+$code = $sran;
+
+$sql = "SELECT * from $tablename WHERE url=\"{$url}\"";
+$result = $conn->query($sql);
+$rows = mysqli_fetch_array($result);
+if($rows["code"] != "")
 {
-	echo $head.$_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$x2.$sran;
+	if($port == "80")
+	{
+		echo $head.$_SERVER["SERVER_NAME"].$x1."?".$rows["code"];
+	}
+	else
+	{
+		echo $head.$_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$x1."?".$rows["code"];
+	}
+	die();
 }
-else
+if($url != "")
 {
-	echo $head.$_SERVER["SERVER_NAME"].$x2.$sran;
+	$sql = "SELECT * from $tablename WHERE url=\"{$url}\"";
+	$result = $conn->query($sql);
+	$rows = mysqli_fetch_array($result);
+	if($rows["code"] != "")
+	{
+		$sql = "DELETE FROM $tablename WHERE url=\"{$url}\"";
+		$conn->query($sql);
+		$code = $code-"1";
+	}
+	$sql = "INSERT INTO $tablename (url,code)VALUES (\"{$url}\",\"{$code}\")";
+	$conn->query($sql);
+	$sql = "SELECT * from $tablename WHERE url=\"{$url}\"";
+	$result = $conn->query($sql);
+	$rows = mysqli_fetch_array($result);
+	if($port == "80")
+	{
+		echo $head.$_SERVER["SERVER_NAME"].$x1."?".$rows["code"];
+	}
+	else
+	{
+		echo $head.$_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$x1."?".$rows["code"];
+	}
 }
+$conn->close();
 ?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+index.php
+用于短网址跳转（当然，如果你的域名太长了，那可能就不叫短网址了）
+这个是隐性跳转的（可能更容易被举报），显性（动态）跳转暂时就不写了
+如果跳转的地址带有get参数，请先将地址转码
+这是搞在数据库的
+懒……
+格式：http(s)://域名(:端口)/要短网址跳转的url
+@xiwangly
+数据库编码推荐utf8mb4
+配置信息要改
+*/
+header("content-type:text/html;charset=utf-8");
+$code = $_SERVER["QUERY_STRING"];
+//配置信息
+//$host = "host(:port)";
+$host = "";
+$username = "";
+$password = "";
+$dbname = "";
+$tablename = "";
+$conn = new mysqli($host,$username,$password,$dbname);
+//设置编码
+$sql = "DEFAULT CHARSET=utf8mb4";
+$conn->query($sql);
+$sql = "SELECT id, url, code FROM $tablename";
+$conn->query($sql);
+$sql = "SELECT * from $tablename WHERE code=\"{$code}\"";
+$result = $conn->query($sql);
+$rows = mysqli_fetch_array($result);
+$url = $rows["url"];
+$conn->close();
+@header("Location: {$url}");
+?>
